1ef6da2936f03e2c26afde28d8a5ba03396cff33
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2020-04-25 19:02:53 +0200

    remove dash from name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='pytorch-train-utils',
+    name='pytorchtrainutils',
     version='0.0.1',
     description='utils for pytorch training',
     url='git@github.com:carloalbertobarbano/pytorch-train-utils.git',
