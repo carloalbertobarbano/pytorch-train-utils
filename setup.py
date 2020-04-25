eb5ef6bd814453ef8f98a9b226f145b78413b0ce
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2020-04-25 18:55:57 +0200

    update url

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='pytorch-train-utils',
     version='0.0.1',
     description='utils for pytorch training',
-    url='none',
+    url='git@github.com:carloalbertobarbano/pytorch-train-utils.git',
     author='Carlo Alberto Barbano'
     author_email='carlo.alberto.barbano@outlook.com',
     license='unlicense',
