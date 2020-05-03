c5c9de646b2bf6c912bcf31241f96ee5351d542d
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2020-05-03 21:07:24 +0200

    add plt show

--- pytorchtrainutils/trainer.py
+++ pytorchtrainutils/trainer.py
@@ -111,8 +111,9 @@ def plot_losses(train, val, test, name, path):
     ax.set_title(name)
     hm = ax.get_figure()
     hm.savefig(path)
-    #hm.clf()
-    #plt.cla()
+    plt.show()
+    hm.clf()
+    plt.cla()
 
 def fit(model, train_dataloader, val_dataloader, test_dataloader, test_every,
         criterion, optimizer, scheduler, metrics, n_epochs, name, path='',
