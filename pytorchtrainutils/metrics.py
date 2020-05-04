6953b6b07405244ed9e04ebd571d5e71dd67cbda
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2020-05-04 15:46:30 +0200

    refactor

--- pytorchtrainutils/metrics.py
+++ pytorchtrainutils/metrics.py
@@ -32,6 +32,28 @@ class Metric:
     def __repr__(self):
         return f'{self.get()}'
 
+class MultilabelMetric(Metric):
+    def __init__(self, apply_sigmoid=True):
+        super().__init__(multiclass=False)
+        self.sigmoid = apply_sigmoid
+    
+    def sigmoid(self, x):
+        return torch.sigmoid(x) if self.sigmoid else x
+
+    def get_class_preds(self, class_idx):
+        outputs = self.sigmoid(self.outputs)
+        targets = self.targets
+
+        class_outputs = outputs[:, class_idx]
+        class_targets = targets[:, class_idx]
+        
+        return class_outputs, class_targets
+    
+    def get_thresholded_preds(self, class_idx, threshold):
+        class_outputs, class_targets = self.get_class_preds(class_idx)
+        class_outputs = (class_outputs > threshold).long()
+        return class_outputs, class_targets
+
 class Accuracy(Metric):
     __name__ = 'acc'
 
@@ -49,70 +71,53 @@ class Accuracy(Metric):
         best_idx = np.argmax(tpr-fpr)
         return thresholds[best_idx]
 
-class MultilabelAccuracy(Metric):
+class MultilabelAccuracy(MultilabelMetric):
     __name__ = 'acc'
     
-    def get(self, threshold=0.5):
+    def get(self):
+        return self.top1_accuracy()
+    
+    def top1_accuracy(self):
         outputs = self.outputs
         _, outputs = torch.max(outputs, 1)
         _, targets = torch.max(self.targets, 1)
         return accuracy_score(targets, outputs.numpy())
-    
-    def class_avg(self, threshold=[0.5]):
-        if len(threshold) == 1:
-            threshold = np.broadcast_to(threshold, self.targets.size(1))
-
-        outputs = torch.sigmoid(self.outputs)
-        outputs = (outputs > torch.tensor(threshold)).long()
-        targets = self.targets
-
-        accuracy = []
-        for class_idx in range(targets.size(1)):
-            class_outputs = outputs[:, class_idx]
-            class_targets = targets[:, class_idx]
-
-            class_acuracy = accuracy_score(class_targets.numpy().astype('uint8'), class_outputs.numpy())
-            accuracy.append(class_acuracy)
         
-        accuracy = np.array(accuracy)
-        return accuracy.mean()
-    
-    def get_best_threshold(self, class_idx):
-        outputs = torch.sigmoid(self.outputs)
-        targets = self.targets
-
-        class_outputs = outputs[:, class_idx]
-        class_targets = targets[:, class_idx]
-
-        fpr, tpr, thresholds = roc_curve(class_targets.numpy(), class_outputs.numpy())
-        best_idx = np.argmax(tpr-fpr)
-        return thresholds[best_idx]
-
-    def get_class_preds(self, class_idx, threshold=0.5):
-        outputs = torch.sigmoid(self.outputs)
-        outputs = (outputs > torch.tensor(threshold)).long()
-        targets = self.targets
-
-        
-        class_outputs = outputs[:, class_idx]
-        class_targets = targets[:, class_idx]
-        
-        return class_outputs, class_targets
-    
     def class_accuracy(self, class_idx, threshold=0.5):
-        class_outputs, class_targets = self.get_class_preds(class_idx, threshold)
+        class_outputs, class_targets = self.get_thresholded_preds(class_idx, threshold)
         class_acuracy = accuracy_score(class_targets.numpy().astype('uint8'), class_outputs.numpy())
         
         return class_acuracy
-    
+
+    def avg_accuracy(self, threshold=[0.5]):
+        if len(threshold) == 1:
+            threshold = np.broadcast_to(threshold, self.targets.size(1))
+
+        accuracy = [self.class_accuracy(idx, threshold[idx]) for idx in range(self.targets.size(1))]
+        return np.mean(accuracy)
+
     def class_ba(self, class_idx, threshold=0.5):
-        class_outputs, class_targets = self.get_class_preds(class_idx, threshold)
+        class_outputs, class_targets = self.get_thresholded_preds(class_idx, threshold)
         class_acuracy = balanced_accuracy_score(class_targets.numpy().astype('uint8'), class_outputs.numpy())
         
         return class_acuracy
     
+    def avg_ba(self, class_idx, threshold=[0.5]):
+        if len(threshold) == 1:
+            threshold = np.broadcast_to(threshold, self.targets.size(1))
+        
+        ba = [self.class_ba(idx, threshold[idx]) for idx in range(self.targets.size(1))]
+        return np.mean(ba)
+    
+    def get_best_threshold(self, class_idx):
+        class_outputs, class_targets = self.get_class_preds(class_idx)
+        fpr, tpr, thresholds = roc_curve(class_targets.numpy(), class_outputs.numpy())
+        best_idx = np.argmax(tpr-fpr)
+
+        return thresholds[best_idx]
+    
     def class_report(self, class_idx, threshold=0.5):
-        class_outputs, class_targets = self.get_class_preds(class_idx, threshold)
+        class_outputs, class_targets = self.get_thresholded_preds(class_idx, threshold)
         report = classification_report(class_targets.numpy().astype('uint8'), class_outputs.numpy())
         
         return report
@@ -130,30 +135,23 @@ class RocAuc(Metric):
     def get_curve(self):
         return roc_curve(self.targets.numpy(), self.outputs.numpy())
 
-class MultilabelRocAuc(Metric):
+class MultilabelRocAuc(MultilabelMetric):
     __name__ = 'col-auc'
     
-    def get_class_preds(self, class_idx):
-        outputs = torch.sigmoid(self.outputs)
-        targets = self.targets
-
-        class_outputs = outputs[:, class_idx]
-        class_targets = targets[:, class_idx]
-
-        return class_outputs, class_targets
-    
     def get(self):
-        roc = []
-        for class_idx in range(self.targets.size(1)):            
-            class_outputs, class_targets = self.get_class_preds(class_idx)
-            class_roc = roc_auc_score(class_targets.numpy(), class_outputs.numpy())
-            roc.append(class_roc)
-                
-        return np.mean(roc)
+        return self.avg_auc()
         
-    def get_curve(self, class_idx):
-        class_outputs,class_targets = self.get_class_preds(class_idx)
+    def class_curve(self, class_idx):
+        class_outputs, class_targets = self.get_class_preds(class_idx)
         return roc_curve(class_targets.numpy(), class_outputs.numpy())
+    
+    def class_auc(self, class_idx):
+        class_outputs, class_targets = self.get_class_preds(class_idx)
+        return roc_auc_score(class_targets.numpy(), class_outputs.numpy())
+    
+    def avg_auc(self):
+        auc = [self.class_auc(class_idx) for class_idx in range(self.targets.size(1))]
+        return np.mean(auc)
 
 class FScore(Metric):
     __name__ = 'f-score'
@@ -164,7 +162,6 @@ class FScore(Metric):
             return f1_score(self.targets.numpy(), outputs.numpy(), average='weighted')
         outputs = (self.outputs > threshold).long()
         return f1_score(self.targets.numpy(), outputs.numpy())
-
 
 class ConfusionMatrix(Metric):
     __name__ = 'cm'
@@ -187,7 +184,7 @@ class ConfusionMatrix(Metric):
 
         return cm
 
-class MultilabelConfusionMatrix(Metric):
+class MultilabelConfusionMatrix(MultilabelMetric):
     __name__ = 'cm'
 
     def __init__(self):
@@ -195,6 +192,9 @@ class MultilabelConfusionMatrix(Metric):
         self.summarizable = False
 
     def get(self, normalized=False):
+        return self.top1_cm(normalized=normalized)
+    
+    def top1_cm(self, normalized=False):
         outputs = self.outputs
         targets = self.targets
         _, outputs = torch.max(self.outputs, 1)
@@ -206,20 +206,9 @@ class MultilabelConfusionMatrix(Metric):
             cm = cm / cm.sum(axis=1)[:, np.newaxis]
 
         return cm
-    
-    def get_class_preds(self, class_idx, threshold=0.5):
-        outputs = torch.sigmoid(self.outputs)
-        outputs = (outputs > torch.tensor(threshold)).long()
-        targets = self.targets
 
-        
-        class_outputs = outputs[:, class_idx]
-        class_targets = targets[:, class_idx]
-        
-        return class_outputs, class_targets
-    
-    def get_class(self, class_idx, normalized=False, threshold=0.5):
-        class_outputs, class_targets = self.get_class_preds(class_idx, threshold=threshold)
+    def class_cm(self, class_idx, normalized=False, threshold=0.5):
+        class_outputs, class_targets = self.get_thresholded_preds(class_idx, threshold=threshold)
         cm = confusion_matrix(class_targets.numpy(), class_outputs.numpy())
         if normalized:
             cm = cm / cm.sum(axis=1)[:, np.newaxis]
